d97d1aa979c2381b1885de76712743649b7e5cc2
root <root@debian>
2017-03-19 22:06:15 +0000

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Simple Deployment</h1>
+    <nav>
+      <a routerLink="/crisis-center" routerLinkActive="active">Crisis Center</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heros</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent { }
