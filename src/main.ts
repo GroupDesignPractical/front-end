4ce36ee35c5f9b7fecb5b2fd3a927c01ae1ffed5
Mark7124 <mark.riley.mwr@hotmail.com>
2017-03-21 22:28:13 +0000

    some graph working

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { HCNGModule } from 'highcharts-ng';
 import { enableProdMode } from '@angular/core';
 // Enable production mode unless running locally
 if (!/localhost/.test(document.location.host)) {
