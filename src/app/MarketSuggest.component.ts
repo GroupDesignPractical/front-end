df82580e9cc98e28425a506bd19b993a97c2e6fa
Mark7124 <mark.riley.mwr@hotmail.com>
2017-04-24 16:12:03 +0100

    Update MarketSuggest.component.ts

--- src/app/MarketSuggest.component.ts
+++ src/app/MarketSuggest.component.ts
@@ -9,12 +9,12 @@ const MARKETS: String[] = [
 ];
 
 @Component({
-  selector: 'suggested-stocks',
+  selector: 'suggested-markets',
   template: `
       <div class="pure-g">
         <div class="pure-u-1 pure-u-md-4-24">
           <p>
-            <label style="padding-left: 10px;">Suggested Markets: </label>
+            <label style="padding-left: 10px;">Suggested Stocks: </label>
           </p>
         </div>
         <div class="pure-u-1 pure-u-md-20-24">
