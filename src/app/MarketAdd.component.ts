bb3d11b6bc502cc4cb4e62cf97aa61a8325a094b
AndreeaGanciulescu <andreeaganciulescu@gmail.com>
2017-04-26 21:26:13 +0100

    Added stock searching functionality
    Allows the user to search through the available stocks using the search
    box. Top three results are displayed. Some allignment/style issues still
    to be fixed.

--- src/app/MarketAdd.component.ts
+++ src/app/MarketAdd.component.ts
@@ -1,20 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit }         from '@angular/core';
+import { Observable }        from 'rxjs/Observable';
+import { Subject }           from 'rxjs/Subject';
+
+// Observable class extensions
+import 'rxjs/add/observable/of';
+
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+
+import {StockAddService} from './StockAdd.service'
+
+
 @Component({
   selector: 'market-add',
+  providers: [StockAddService],
   template: `
-      <div class="pure-g">
+    <div class="pure-g">
         <div class="pure-u-1 pure-u-md-3-24" id="market-search-label">
           <label>Add Stock: </label>
         </div>
         <div class="pure-u-1 pure-u-md-21-24">
-          <div id="market-search-box">
+          <div id="stock-search-box">
             <div class="pure-u-1-24"></div>
-            <input [(ngModel)]="newMarket" placeholder="e.g. Nikkei 225" class="pure-u-22-24">
+            <input [(ngModel)]="newStock" placeholder="e.g. Nikkei 225" class="pure-u-22-24" (keyup) = "search(newStock)">
+			<div>
+				<div *ngFor = "let stock of stocks | async"
+					(click) = "addStock(stock)" id = "stock-search-result" >
+					{{stock}}
+			</div>
           </div>
         </div>
       </div>
   `
 })
-export class MarketAddComponent {
-  newMarket = '';
+export class MarketAddComponent implements OnInit{
+
+    stocks: Observable<string[]>;
+	private searchTerms = new Subject<string>();
+	
+	constructor(
+		private stockAddService: StockAddService){}
+
+	search(term: string): void{
+		this.searchTerms.next(term);
+	}
+	
+	addStock() : void{
+		console.log("Added!")
+	}
+	
+	newStock = '';
+	
+	ngOnInit(){
+		this.stocks = this.searchTerms
+			.debounceTime(50) //wait 50 ms after keystroke
+			.distinctUntilChanged()
+			.switchMap(term => term
+				? this.stockAddService.search(term)
+				:Observable.of<string[]>([]))
+			.catch(error => {return Observable.of<string[]>([]);}); //TODO
+		
+	}
 }
