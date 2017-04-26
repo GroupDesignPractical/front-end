ec3d777e883b1983cbc6050fad2da5fd94653648
Nick Hu <me@nickhu.co.uk>
2017-04-26 18:24:34 +0100

    Fix production mode check for local VMs

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 // Enable production mode unless running locally
-if (!/localhost/.test(document.location.host)) {
+if (!(/localhost/.test(document.location.host) || /192.168.99.100/.test(document.location.host))) {
   enableProdMode();
 }
 platformBrowserDynamic().bootstrapModule(AppModule);
