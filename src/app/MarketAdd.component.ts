bdc2f53922e331bac2b88bef9276ee403412cf24
Mark7124 <mark.riley.mwr@hotmail.com>
2017-05-01 11:57:15 +0100

    Added missing /div, tidied indentation

--- src/app/MarketAdd.component.ts
+++ src/app/MarketAdd.component.ts
@@ -19,20 +19,21 @@ import {StockAddService} from './StockAdd.service'
   providers: [StockAddService],
   template: `
     <div class="pure-g">
-        <div class="pure-u-1 pure-u-md-3-24" id="market-search-label">
-          <label>Add Stock: </label>
-        </div>
-        <div class="pure-u-1 pure-u-md-21-24">
-          <div id="stock-search-box">
-            <div class="pure-u-1-24"></div>
-            <input [(ngModel)]="newStock" placeholder="e.g. Nikkei 225" class="pure-u-22-24" (keyup) = "search(newStock)">
-			<div>
-				<div *ngFor = "let stock of stocks | async" >
-				{{stock}}
-			</div>
+      <div class="pure-u-1 pure-u-md-3-24" id="market-search-label">
+        <label>Add Stock: </label>
+      </div>
+      <div class="pure-u-1 pure-u-md-21-24">
+        <div id="stock-search-box">
+          <div class="pure-u-1-24"></div>
+          <input [(ngModel)]="newStock" placeholder="e.g. Nikkei 225" class="pure-u-22-24" (keyup) = "search(newStock)">
+			    <div>
+				    <div *ngFor = "let stock of stocks | async" >
+				      {{stock}}
+			      </div>
           </div>
         </div>
       </div>
+    </div>
   `
 })
 export class MarketAddComponent implements OnInit{
